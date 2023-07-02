51dca9b86dba0389a34ffcd2d508c506751e569b
shinnu73 <111646417+shinnu73@users.noreply.github.com>
2023-07-02 23:21:06 +0530

    Create atg_world_unit_test.py

---
+++ atg_world_unit_test.py
@@ -0,0 +1,9 @@
+import requests
+def test_website_loads_properly():
+    response = requests.get("https://atg.world")
+    assert response.status_code == 200
+def test_website_loads_properly():
+    print("Starting website load test...")
+    response = requests.get("https://atg.world")
+    assert response.status_code == 200
+    print("Website loaded successfully!")
