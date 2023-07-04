a9aea7c3ef156d790467c695f82fd04d290e25fe
shinnu73 <111646417+shinnu73@users.noreply.github.com>
2023-07-04 14:02:11 +0530

    Update atg_world_unit_test.py

--- atg_world_unit_test.py
+++ atg_world_unit_test.py
@@ -1,6 +1,6 @@
 import requests
 def test_website_loads_properly():
-    response = request.get("https://atg.world")
+    response = requests.get("https://atg.world")
     assert response.status_code == 200
 def test_website_loads_properly():
     print("Starting website load test...")
