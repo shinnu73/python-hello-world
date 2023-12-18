f716a69201dee20cf20d6bd18446115d0d2143b4
shinnu73 <111646417+shinnu73@users.noreply.github.com>
2023-12-18 21:45:09 +0530

    Update atg_world_unit_test.py

--- atg_world_unit_test.py
+++ atg_world_unit_test.py
@@ -4,16 +4,21 @@ from selenium import webdriver
 class TestATGWebsite(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.get("https://atg.world")
+        try:
+            self.driver = webdriver.Chrome()
+    
+            self.driver.get("https://atg.world")
+        except Exception as e:
+            self.fail(f"Error in setUp: {e}")
 
-    def test_website_loading(self):
+    def test_website_title_matches_expected(self):
         title = self.driver.title
-        self.assertEqual(title, "ATG.World")
+        self.assertEqual(title, "ATG.World", "Website title does not match expected value.")
 
     def tearDown(self):
         self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
+
    
