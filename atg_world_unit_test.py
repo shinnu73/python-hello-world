0b709d29b352fa04d2383c8e2df918b027a4f81e
shinnu73 <111646417+shinnu73@users.noreply.github.com>
2023-12-18 21:56:25 +0530

    Update atg_world_unit_test.py

--- atg_world_unit_test.py
+++ atg_world_unit_test.py
@@ -3,22 +3,23 @@ from selenium import webdriver
 
 class TestATGWebsite(unittest.TestCase):
 
-    def setUp(self):
+    def test_website_loading(self):
         try:
-            self.driver = webdriver.Chrome()
-    
-            self.driver.get("https://atg.world")
+            # Initialize the WebDriver (Chrome)
+            driver = webdriver.Chrome()
+
+            # Navigate to the ATG.World website
+            driver.get("https://atg.world")
+
+            # Check if the title contains "ATG.World" (you can modify this check based on your requirement)
+            self.assertIn("ATG.World", driver.title, "Website did not load successfully.")
         except Exception as e:
-            self.fail(f"Error in setUp: {e}")
-
-    def test_website_title_matches_expected(self):
-        title = self.driver.title
-        self.assertEqual(title, "ATG.World", "Website title does not match expected value.")
-
-    def tearDown(self):
-        self.driver.quit()
+            # Fail the test if there's an exception
+            self.fail(f"Error: {e}")
+        finally:
+            # Quit the WebDriver (close the browser) in the finally block to ensure it always happens
+            if driver:
+                driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
-
-   
